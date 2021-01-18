a4a32919d9c28599221cec6433e0a3e3ebef03b1
raphael.vasconcelos <raphael.vasconcelos01@gmail.com.br>
2021-01-18 19:40:48 -0300

    Implementando a autenticação com Auth0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,32 @@
 import { NextPage } from 'next';
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 
 import Nav from '../components/nav';
 
 const IndexPage: NextPage = () => {
+  const [session, loading] = useSession()
+
   return (
     <div>
       <Nav />
-      <div className="py-60">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          Next.js + Tailwind CSS 2.0
-        </h1>
-      </div>
+      {!session && (
+        <div className="text-3xl">
+          Not signed in <br />
+          <button onClick={(): Promise<void> => signIn("auth0")}>Sign in</button>
+        </div>
+      )}
+      {session && (
+        <div className="text-3xl">
+          Signed in as {session.user.email} <br />
+          <button onClick={(): Promise<void> => signOut()}>Sign out</button>
+        </div>
+      )}
+      {loading && (
+        <div className="text-5xl">
+          <h1>CARREGANDO</h1>
+        </div>
+      )}
     </div>
   )
 }
